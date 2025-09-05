59cc39bfda3e9719abc747c4563520946481ed78
serhw <fakemail@noemail.com>
2025-09-05 10:43:02 +0300

    configuration for logger

--- Subtitles.Api/Subtitles.Api/Program.cs
+++ Subtitles.Api/Subtitles.Api/Program.cs
@@ -1,15 +1,40 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341") // Default Seq URL
+    .CreateLogger();
 
+// Use Serilog
+builder.Host.UseSerilog();
+
+// Add services to the container.
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSerilogRequestLogging(); // Add request logging
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+try
+{
+    Log.Information("Starting the application");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
